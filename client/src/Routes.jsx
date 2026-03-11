0ebaae920c0f67bf8cd9fe02b8c50c421d8bf7fa
shivanis-rao <shivani.s@nineleaps.com>
2026-03-11 16:35:52 +0530

    FEAT:Login successful

---
+++ client/src/Routes.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+
+
+const AppRoutes = () => {
+ return (
+   <BrowserRouter>
+     <Routes>
+       <Route path="/login" element={<LoginPage />} />
+     </Routes>
+   </BrowserRouter>
+ );
+};
+
+
+export default AppRoutes;
+
